891e9e2014c71cde49a816b277776ee059d7034c
Eslim Daga <esliim9811@gmail.com>
2021-03-10 11:56:25 -0500

    Add url index

--- django_init/urls.py
+++ django_init/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from applications.myapp.views import init
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',init ,name = 'index' )
 ]
