eacea0d925bd26ec85d116c0c1200fd16d8d9b1c
Eslim Daga <esliim9811@gmail.com>
2021-03-10 11:54:48 -0500

    Add Model Person

--- applications/myapp/models.py
+++ applications/myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+  id = models.AutoField(primary_key = True)
+  firstname = models.CharField(max_length = 100)
+  lastname = models.CharField(max_length = 120)
+  email = models.EmailField(max_length = 200)
+
+  def __str__(self):
+    return self.firstname
